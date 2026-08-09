75040028382db825b34f686862e205099af721d3
Michael Rockson <michaelrockszn@gmail.com>
2026-08-09 12:57:22 +0000

    feat(frontend): add settings form

--- leema-frontend/src/App.tsx
+++ leema-frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
-import {Section} from "@/components/ui/section.tsx";
+import { Section } from "@/components/ui/section.tsx";
 import Header from "@/features/Header/Header.tsx";
 import ResearchTable from "@/features/ResearchTable/ResearchTable.tsx";
 import FindingsTable from "@/features/FindingsTable/components/FindingsTable.tsx";
 
 function App() {
-    return (
-        <Section>
-            <Header/>
-            <ResearchTable/>
-            <FindingsTable/>
-        </Section>
-    );
+  return (
+    <Section>
+      <Header />
+      <ResearchTable />
+      <FindingsTable />
+    </Section>
+  );
 }
 
 export default App;
--- leema-frontend/src/components/DrawerStore.tsx
+++ leema-frontend/src/components/DrawerStore.tsx
@@ -1,48 +1,61 @@
 import * as React from "react";
 import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
-} from "@/components/ui/drawer.tsx"
-import {Button} from "@/components/ui/button.tsx";
-import type {ButtonProps} from "@base-ui/react";
-
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import type { ButtonProps } from "@base-ui/react";
 
 type DrawerStore = {
-    button: React.ComponentType<ButtonProps>;
-    ButtonLabel: string | React.ComponentType<{ size?: number; color?: string }> | React.ReactElement;
-    drawerTitle: string;
-    drawerContent: React.ReactElement | null;
-    drawerSaveButtonLabel: string;
-    drawerCancelButtonLabel: string;
-}
+  button: React.ComponentType<ButtonProps>;
+  ButtonLabel:
+    | string
+    | React.ComponentType<{ size?: number; color?: string }>
+    | React.ReactElement;
+  drawerTitle: string;
+  drawerContent: React.ReactElement | null;
+  drawerSaveButtonLabel: string;
+  drawerCancelButtonLabel: string;
+};
 
-export default function DrawerStore({drawerProps}: { drawerProps: DrawerStore }) {
-    const {ButtonLabel} = drawerProps;
+export default function DrawerStore({
+  drawerProps,
+}: {
+  drawerProps: DrawerStore;
+}) {
+  const { ButtonLabel } = drawerProps;
 
-    const renderButtonLabel = () => {
-        if (typeof ButtonLabel === 'string') return ButtonLabel;
-        if (React.isValidElement(ButtonLabel)) return ButtonLabel;
-        return <ButtonLabel />;
-    };
+  const renderButtonLabel = () => {
+    if (typeof ButtonLabel === "string") return ButtonLabel;
+    if (React.isValidElement(ButtonLabel)) return ButtonLabel;
+    return <ButtonLabel />;
+  };
 
-    return (
-        <Drawer swipeDirection="right">
-            <DrawerTrigger> {renderButtonLabel()}</DrawerTrigger>
-            <DrawerContent>
-                <DrawerHeader className="border-b py-4">
-                    <DrawerTitle><h3>{drawerProps.drawerTitle}</h3></DrawerTitle>
-                </DrawerHeader>
-                <div className="flex-1 overflow-y-auto p-4">{drawerProps.drawerContent}</div>
-                <DrawerFooter>
-                    <Button>{drawerProps.drawerSaveButtonLabel}</Button>
-                    <DrawerClose render={<Button variant="outline"/>}> {drawerProps.drawerCancelButtonLabel} </DrawerClose>
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
-    );
+  return (
+    <Drawer swipeDirection="right">
+      <DrawerTrigger>{renderButtonLabel()}</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="border-b py-4">
+          <DrawerTitle>
+            <h3>{drawerProps.drawerTitle}</h3>
+          </DrawerTitle>
+        </DrawerHeader>
+        <div className="flex-1 overflow-y-auto p-4">
+          {drawerProps.drawerContent}
+        </div>
+        <DrawerFooter className="flex jus">
+          <Button>{drawerProps.drawerSaveButtonLabel}</Button>
+          <DrawerClose render={<Button variant="outline" />}>
+            {" "}
+            {drawerProps.drawerCancelButtonLabel}{" "}
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
 }
